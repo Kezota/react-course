101993b4c63cab93092852a161efe58465b3acb8
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2024-09-25 23:50:05 +0700

    added delete cabin functionality

--- jonas-react-course/17-the-wild-oasis/src/App.jsx
+++ jonas-react-course/17-the-wild-oasis/src/App.jsx
@@ -16,7 +16,8 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      stateTime: 60 * 1000,
+      // stateTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
