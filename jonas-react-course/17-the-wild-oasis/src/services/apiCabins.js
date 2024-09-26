bd250061cde0170d543cc07b812b3e3bffbba1b8
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2024-09-26 11:22:15 +0700

    added createCabin functionality

--- jonas-react-course/17-the-wild-oasis/src/services/apiCabins.js
+++ jonas-react-course/17-the-wild-oasis/src/services/apiCabins.js
@@ -11,6 +11,26 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.log(error);
+    throw new Error("An error occurred while create the cabin.");
+  }
+}
+
+export async function updateCabin() {
+  const { data, error } = await supabase
+    .from("cabins")
+    .update({ other_column: "otherValue" })
+    .eq("some_column", "someValue")
+    .select();
+}
+
 export async function deleteCabin(id) {
   const { error } = await supabase.from("cabins").delete().eq("id", id);
 
